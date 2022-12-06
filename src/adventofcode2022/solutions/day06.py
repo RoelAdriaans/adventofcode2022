dd4bf9a75f586092bd630395154fe787ba64c610
Roel Adriaans <roel.adriaans@asml.com>
2022-12-06 19:46:01 +0100

    Day 6, simplyfied

--- src/adventofcode2022/solutions/day06.py
+++ src/adventofcode2022/solutions/day06.py
@@ -2,31 +2,23 @@ from adventofcode2022.utils.abstract import FileReaderSolution
 
 
 class Day06:
-    pass
+    @staticmethod
+    def parse_packets(input_data: str, length: int) -> int:
+        input_data = input_data.strip()
+        for start in range(0, len(input_data)):
+            packet = input_data[start : start + length]
+            if len(set(packet)) != length:
+                # Duplicated characters
+                continue
+            # We have something new, return starting position + 4 chars in this packet
+            return start + length
 
 
 class Day06PartA(Day06, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        input_data = input_data.strip()
-        for start in range(0, len(input_data)):
-            packet = input_data[start : start + 4]
-            if len(set(packet)) != 4:
-                # Duplicated characters
-                continue
-            # We have something new, return starting position + 4 chars in this packet
-            return start + 4
-
-        return -1
+        return self.parse_packets(input_data, 4)
 
 
 class Day06PartB(Day06, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        input_data = input_data.strip()
-        for start in range(0, len(input_data)):
-            packet = input_data[start : start + 14]
-            if len(set(packet)) != 14:
-                # Duplicated characters
-                continue
-            # We have something new, return starting position + 4 chars in this packet
-            return start + 14
-        return -1
+        return self.parse_packets(input_data, 14)
