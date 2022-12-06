fc47ceac7a4be342cb406af6ed222482c23d6dc0
Roel Adriaans <roel.adriaans@ordina.nl>
2022-12-06 21:42:40 +0100

    Implement day 6 with more_itertools.sliding_window

--- src/adventofcode2022/solutions/day06.py
+++ src/adventofcode2022/solutions/day06.py
@@ -1,3 +1,5 @@
+from more_itertools import sliding_window
+
 from adventofcode2022.utils.abstract import FileReaderSolution
 
 
@@ -5,13 +7,14 @@ class Day06:
     @staticmethod
     def parse_packets(input_data: str, length: int) -> int:
         input_data = input_data.strip()
-        for start in range(0, len(input_data)):
-            packet = input_data[start : start + length]
+        for idx, packet in enumerate(sliding_window(input_data, length)):
             if len(set(packet)) != length:
                 # Duplicated characters
                 continue
             # We have something new, return starting position + 4 chars in this packet
-            return start + length
+            return idx + length
+
+        raise ValueError("No package found for length %s", length)
 
 
 class Day06PartA(Day06, FileReaderSolution):
