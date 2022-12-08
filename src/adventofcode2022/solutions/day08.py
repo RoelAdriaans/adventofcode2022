33ec7a86503109177fb6d3d7df7de51288d09db7
Roel Adriaans <roel.adriaans@asml.com>
2022-12-08 10:12:25 +0100

    Day 08, part b

--- src/adventofcode2022/solutions/day08.py
+++ src/adventofcode2022/solutions/day08.py
@@ -1,3 +1,5 @@
+import math
+
 from adventofcode2022.utils.abstract import FileReaderSolution
 from adventofcode2022.utils.point import XYPoint as Point
 
@@ -70,5 +72,44 @@ class Day08PartA(Day08, FileReaderSolution):
 
 
 class Day08PartB(Day08, FileReaderSolution):
+    def visible_trees(self, point: Point) -> int:
+        """Count how many trees are visible from this position,
+        and multiply their numbers together."""
+        return math.prod(
+            self.visible_points_for_direction(point, dx, dy)
+            for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1))
+        )
+
+    def visible_points_for_direction(self, point: Point, dx: int, dy: int) -> int:
+        """How many points are visible from this direction?"""
+        value = self.grid[point]
+
+        test_x = point.x + dx
+        test_y = point.y + dy
+        visible = 0
+        while True:
+            test_value = self.grid.get(Point(test_x, test_y), -1)
+            if test_value == -1:
+                # On the edge, we did not find any higher points
+                # There is nothing beyond the edge, to not count
+                return visible
+            elif test_value >= value:
+                # We are now looking at a higher tree. Also count this tree
+                return visible + 1
+            test_x += dx
+            test_y += dy
+            visible += 1
+
+    def count_visible(self) -> int:
+        """Count the visible trees"""
+        max_visible = 0
+
+        for x in range(self.min_x, self.max_x + 1):
+            for y in range(self.min_y, self.max_y + 1):
+                # Check if there is a tree lower than `value` in all directions
+                max_visible = max(max_visible, self.visible_trees(Point(x, y)))
+        return max_visible
+
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        self.parse(input_data)
+        return self.count_visible()
--- tests/solutions/test_day08_b.py
+++ tests/solutions/test_day08_b.py
@@ -1,19 +1,37 @@
-import pytest
+from textwrap import dedent
 
 from adventofcode2022.solutions.day08 import Day08PartB
+from adventofcode2022.utils.point import XYPoint as Point
 
 
 class TestDay08PartB:
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day08b_solve(self, input_data, expected_result):
-        solution = Day08PartB()
-        result = solution.solve(input_data)
-        assert result == expected_result
+    test_data = """\
+    30373
+    25512
+    65332
+    33549
+    35390
+    """
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
+    def test_day08b_for_point(self):
+        solution = Day08PartB()
+        solution.parse(dedent(self.test_data))
+        # Point 5 has a store of 4
+        tree = Point(1, 2)
+        assert solution.visible_points_for_direction(tree, 0, -1) == 1  # Left
+        assert solution.visible_points_for_direction(tree, 0, 1) == 2  # Right
+        assert solution.visible_points_for_direction(tree, -1, 0) == 1  # Up
+        assert solution.visible_points_for_direction(tree, 1, 0) == 2  # Down
+        # And 1 * 1 * 2 * 2 == 4
+        assert solution.visible_trees(tree) == 4
+
+    def test_day08b_solve(self):
+        solution = Day08PartB()
+        result = solution.solve(dedent(self.test_data))
+        assert result == 8
+
     def test_day08b_data(self):
         """Result we got when we did the real solution"""
         solution = Day08PartB()
         res = solution("day_08/day08.txt")
-        assert res == 0
+        assert res == 172224
