e344b91e05c8e3e7bdb3a01fa2227de871aac3db
Roel Adriaans <roel.adriaans@ordina.nl>
2022-12-10 14:52:31 +0100

    WIP day 9

--- src/adventofcode2022/solutions/day09.py
+++ src/adventofcode2022/solutions/day09.py
@@ -24,13 +24,15 @@ class Day09:
                 elif move == "L":
                     head.x -= 1
                 elif move == "D":
-                    head.y -= 1
-                elif move == "U":
                     head.y += 1
+                elif move == "U":
+                    head.y -= 1
 
                 # Define steps for the tail to move:
                 horizontal_move = head.x == tail.x or head.y == tail.y
-                if horizontal_move and tail.x > head.x and tail.y == head.y:
+                if tail.distance(head)== 1:
+                    pass
+                elif horizontal_move and tail.x > head.x and tail.y == head.y:
                     tail.x -= 1
                 elif horizontal_move and tail.x < head.x and tail.y == head.y:
                     tail.x += 1
@@ -52,7 +54,34 @@ class Day09:
                     tail.y -= 1
 
                 tail_positions.add((tail.x, tail.y))
+                self.print(head, tail, tail_positions)
+
         return len(tail_positions)
+
+    @staticmethod
+    def print(head: Point, tail: Point, tail_positions: set[tuple[int, int]]):
+        min_x = min(head.x, tail.x)
+        min_y = min(head.y, tail.y)
+        max_x = max(head.x, tail.x)
+        max_y = max(head.y, tail.y)
+        line = "\n\n"
+        for y in range(min_y, max_y + 1):
+            for x in range(min_x, max_x + 1):
+                pnt = Point(x, y)
+                if head == tail == pnt:
+                    line += "*"
+                elif head == pnt and head != tail:
+                    line += "H"
+                elif tail == pnt and head != tail:
+                    line += "T"
+                elif (x, y) in tail_positions:
+                    line += "#"
+                else:
+                    line += "."
+            line += "\n"
+        line += "\n\n"
+        print(line)
+
 
 class Day09PartA(Day09, FileReaderSolution):
     def solve(self, input_data: str) -> int:
--- src/adventofcode2022/utils/point.py
+++ src/adventofcode2022/utils/point.py
@@ -13,6 +13,11 @@ class XYZPoint:
             raise ValueError("Cannot add incompatible types together")
         return XYZPoint(x=self.x + other.x, y=self.y + other.y, z=self.z + other.z)
 
+    def __iter__(self):
+        yield self.x
+        yield self.y
+        yield self.z
+
     def distance(self, other):
         return abs(self.x - other.x) + abs(self.y - other.y) + abs(self.z - other.z)
 
@@ -26,3 +31,10 @@ class XYPoint:
         if not issubclass(type(other), XYPoint):
             raise ValueError("Cannot add incompatible types together")
         return XYPoint(x=self.x + other.x, y=self.y + other.y)
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+
+    def distance(self, other):
+        return abs(self.x - other.x) + abs(self.y - other.y)
