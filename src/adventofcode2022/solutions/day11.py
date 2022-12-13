308c66f571bd0ceef44f73445d9e897299636e93
Roel Adriaans <roel.adriaans@ordina.nl>
2022-12-13 21:41:01 +0100

    Implement day 11, part 1

--- src/adventofcode2022/solutions/day11.py
+++ src/adventofcode2022/solutions/day11.py
@@ -1,24 +1,39 @@
 from __future__ import annotations
 
+import math
+from typing import NamedTuple
+
 import parse
 
 from adventofcode2022.utils.abstract import FileReaderSolution
+from adventofcode2022.utils.queue import Queue
+
+
+class Throw(NamedTuple):
+    monkid: int
+    worry_level: int
 
 
 class Monkey:
     monkid: int
-    items: list[int]
+    items: Queue[int]
     operation: str
 
     test_division: int
     true_to: int
     false_to: int
 
+    inspections: int
+
     def __init__(self):
-        self.items = []
+        self.items = Queue()
+        self.inspections = 0
 
     def __repr__(self):
-        return f"Monkey(id:{self.monkid}, items[{', '.join(str(i) for i in self.items)}])"
+        return (
+            f"Monkey(id:{self.monkid}, inspections:{self.inspections}, "
+            f"items[{repr(self.items)}])"
+        )
 
     @staticmethod
     def from_string(data: str) -> Monkey:
@@ -26,10 +41,35 @@ class Monkey:
         monkey = Monkey()
 
         monkey.monkid = parse.parse("Monkey {:d}:", lines[0])[0]
-        monkey.items = [q[0] for q in parse.findall("{:d}", lines[1])]
-        monkey.operation = lines[2].split("=")[1].strip()
+        for item in parse.findall("{:d}", lines[1]):
+            monkey.items.push(item[0])
 
+        monkey.operation = lines[2].split("=")[1].strip()
+        monkey.test_division = parse.parse("Test: divisible by {:d}", lines[3])[0]
+        monkey.true_to = parse.parse("If true: throw to monkey {:d}", lines[4])[0]
+        monkey.false_to = parse.parse("If false: throw to monkey {:d}", lines[5])[0]
         return monkey
+
+    def inspect(self) -> list[Throw]:
+        """Inspect the items we have. Returns a list of Throw's"""
+        throws: list[Throw] = []
+        while self.items:
+            self.inspections += 1
+            item = self.items.pop()
+            # Inspect an item
+            # Appla operation
+            new = self._apply_operation(item)
+            # Divide by 3
+            new //= 3
+            if new % self.test_division == 0:
+                throws.append(Throw(self.true_to, new))
+            else:
+                throws.append(Throw(self.false_to, new))
+        return throws
+
+    def _apply_operation(self, item):
+        action = self.operation.replace("old", str(item))
+        return eval(action)
 
 
 class Day11:
@@ -40,12 +80,26 @@ class Day11:
         monkeys = [Monkey.from_string(string) for string in input_data.split("\n\n")]
         return monkeys
 
+    def play_rounds(self, rounds: int):
+        for i in range(rounds):
+            self.play()
+
+    def play(self):
+        for monkey in self.monkeys:
+            throws = monkey.inspect()
+            # And now distribute the throws around
+            for throw in throws:
+                self.monkeys[throw.monkid].items.push(throw.worry_level)
+
+    def compute_score(self):
+        return math.prod(sorted(list(m.inspections for m in self.monkeys))[-2:])
+
 
 class Day11PartA(Day11, FileReaderSolution):
     def solve(self, input_data: str) -> int:
         self.monkeys = self.parse(input_data)
-
-        return -1
+        self.play_rounds(20)
+        return self.compute_score()
 
 
 class Day11PartB(Day11, FileReaderSolution):
--- tests/solutions/test_day11_a.py
+++ tests/solutions/test_day11_a.py
@@ -1,22 +1,20 @@
 from textwrap import dedent
 
-import pytest
-
 from adventofcode2022.solutions.day11 import Day11PartA, Monkey
 
 
 class TestDay11PartA:
     def test_monkey(self):
-        test_string = dedent("""\
+        test_string = """\
         Monkey 0:
           Starting items: 79, 98
           Operation: new = old * 19
           Test: divisible by 23
             If true: throw to monkey 2
             If false: throw to monkey 3
-        """)
+        """
 
-        monkey = Monkey.from_string(test_string)
+        monkey = Monkey.from_string(dedent(test_string))
         assert monkey.monkid == 0
         assert monkey.items == [79, 98]
         assert monkey.operation == "old * 19"
@@ -24,15 +22,13 @@ class TestDay11PartA:
         assert monkey.false_to == 3
         assert monkey.test_division == 23
 
-
     def test_day11a_solve(self, testdata):
         solution = Day11PartA()
         result = solution.solve(testdata)
         assert result == 10605
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day11a_data(self):
         """Result we got when we did the real solution"""
         solution = Day11PartA()
         res = solution("day_11/day11.txt")
-        assert res == 0
+        assert res == 120056
