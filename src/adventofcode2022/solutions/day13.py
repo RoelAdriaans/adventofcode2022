5bf2749782ccf368e5a2d582fa059f3f9498da49
Roel Adriaans <roel.adriaans@ordina.nl>
2022-12-29 23:05:08 +0100

    Solve day 13, part 1

--- src/adventofcode2022/solutions/day13.py
+++ src/adventofcode2022/solutions/day13.py
@@ -1,99 +1,90 @@
 from __future__ import annotations
 
-from collections import deque
+from ast import literal_eval
 
 from adventofcode2022.utils.abstract import FileReaderSolution
+
+Packet = list[int | list]
 
 
 class Pair:
     left: Packet
     right: Packet
+    index: int
 
-    def __init__(self, left: Packet, right: Packet):
+    def __init__(self, left: Packet, right: Packet, index: int):
         self.left = left
         self.right = right
+        self.index = index
 
     def is_valid(self) -> bool:
         """Validate if a packet is has valid pairs"""
-        return False
+        return self._compare(self.left, self.right) == 1
 
+    def _compare(self, left: Packet | int, right: Packet | int) -> int:
+        # Convert to list if needed
+        if isinstance(right, int) and isinstance(left, int):
+            if left < right:
+                # Right Order
+                return 1
+            if left == right:
+                # Same, continue checking
+                return 0
+            if left > right:
+                # Not right
+                return -1
 
-class Packet:
-    parent: Packet
-    left: Packet | None
-    right: Packet | None
-    content: list[int]
+        if isinstance(left, int):
+            left = [left]
+        if isinstance(right, int):
+            right = [right]
 
-    def __init__(
-        self, left: Packet | None = None, right: Packet | None = None, content=None
-    ):
-        self.left = left
-        self.right = right
-        if content is None:
-            self.content = []
-        else:
-            self.content = content
+        # Let's dive into this! left and right have now both been turned
+        # into lists
+        for idx, left_value in enumerate(left):
+            try:
+                right_value = right[idx]
+            except IndexError:
+                # Until now, all the values have been the same.
+                # Right side ran out of items,
+                # so inputs are not in the right order
+                return -1
 
-    def __repr__(self):
-        if len(self.content) > 1:
-            return ",".join(str(i) for i in self.content)
+            res = self._compare(left_value, right_value)
+            if res != 0:
+                return res
 
-        left = repr(self.left) if self.left else None
-        right = repr(self.right) if self.right else None
-        if left and right:
-            return f"[{left},{right}]"
-        else:
-            return f"[{left or right}]"
+        # All the values are the same, we have to dive deeper
+        if len(left) == len(right):
+            # Left and right arrays are the same length, and we did not find
+            # any wrong digits. Return 0 to contue comparing
+            return 0
+        # If the left list runs out of items first, the inputs are in
+        # the right order
+        if len(left) < len(right):
+            return 1
+
+        # If we are here, and all has failed, just return 0
+        return 0
 
 
 class Day13:
-    def parse(self, queue: deque[str]) -> Packet:
-        if queue[0] == "[":
-            # Remove [
-            queue.popleft()
-            left = self.parse(queue)
-            # Remove ]
-            queue.popleft()
-            right = self.parse(queue)
-            # Remove ]
-            queue.popleft()
+    def parse(self, string_pair: str, index: int) -> Pair:
+        p1, p2 = string_pair.splitlines()
+        left: Packet = literal_eval(p1)
+        right: Packet = literal_eval(p2)
 
-            # Create a new Packet
-            new_packet = Packet(left=left, right=right)
-            left.parent = new_packet
-            right.parent = new_packet
-            return new_packet
-        else:
-            # Next digit is one, or more, numbers
-            digits = []
-            while queue:
-                current_digit = []
-                while queue[0].isdigit():
-                    current_digit.append(queue.popleft())
-                if current_digit:
-                    digit = int("".join(map(str, current_digit)))
-                    digits.append(digit)
-
-                if queue[0] == "]":
-                    # We have reached the end of the digits
-                    break
-                if queue[0] == ",":
-                    queue.popleft()
-
-            return Packet(content=digits)
+        return Pair(left=left, right=right, index=index)
 
 
 class Day13PartA(Day13, FileReaderSolution):
     def solve(self, input_data: str) -> int:
         pairs: list[Pair] = []
 
-        for input_pair in input_data.split("\n\n"):
-            left = Packet(self.parse(deque(input_pair[0])))
-            right = Packet(self.parse(deque(input_pair[1])))
-            pair = Pair(left=left, right=right)
-            pairs.append(pair)
+        for index, input_pair in enumerate(input_data.split("\n\n"), start=1):
+            pairs.append(self.parse(input_pair, index))
 
-        valid_pairs = sum(p.is_valid() for p in pairs)
+        valid_pairs = sum(p.index for p in pairs if p.is_valid())
 
         return valid_pairs
 
--- tests/solutions/test_day13_a.py
+++ tests/solutions/test_day13_a.py
@@ -1,28 +1,9 @@
-from collections import deque
-
 import pytest
 
 from adventofcode2022.solutions.day13 import Day13PartA
 
 
 class TestDay13PartA:
-    @pytest.mark.parametrize(
-        "input_string",
-        (
-            "[[1,2],[3,4]]",
-            # "[]",
-            # "[9]",
-            # "[9]",
-            # "[1, 1, 3, 1, 1]",
-            # "[[1],[2,3,4]]",
-            # "[1,[2,[3,[4,[5,6,7]]]],8,9]",
-        ),
-    )
-    @pytest.mark.skip(reason="Not yet implemented")
-    def test_day13a_parsing(self, input_string):
-        solution = Day13PartA()
-        assert repr(solution.parse(deque(input_string))) == input_string
-
     @pytest.mark.parametrize(
         ("pair", "valid"),
         [
@@ -36,7 +17,6 @@ class TestDay13PartA:
             (8, False),
         ],
     )
-    @pytest.mark.skip(reason="Not yet implemented")
     def test_day13a_individual_pairs(self, testdata, pair, valid):
         pairs = testdata.split("\n\n")
 
@@ -44,15 +24,17 @@ class TestDay13PartA:
         result = solution.solve(pairs[pair - 1])
         assert result == valid
 
-    @pytest.mark.skip(reason="Not yet implemented")
     def test_day13a_solve(self, testdata):
         solution = Day13PartA()
         result = solution.solve(testdata)
         assert result == 13
 
-    @pytest.mark.skip(reason="Not yet implemented")
     def test_day13a_data(self):
         """Result we got when we did the real solution"""
         solution = Day13PartA()
         res = solution("day_13/day13.txt")
-        assert res == 0
+        assert res > 1008
+        assert res > 3364
+        assert res != 5738
+        assert res < 7750
+        assert res == 6420
