31d0ddf8793bf8656acb3d4ad88cbf5328d25149
Roel Adriaans <roel.adriaans@ordina.nl>
2023-01-08 20:48:20 +0100

    WIP

--- src/adventofcode2022/solutions/day15.py
+++ src/adventofcode2022/solutions/day15.py
@@ -2,20 +2,23 @@ from parse import parse
 
 from adventofcode2022.utils.abstract import FileReaderSolution
 from adventofcode2022.utils.point import XYPoint as Point
+from typing import NamedTuple
+
+
+class Line(NamedTuple):
+    sensor: Point
+    beacon: Point
+    distance: int
 
 
 class Day15:
-    sensors: list[Point]
-    beacons: list[Point]
-    distance: list[int]
+    lines: list[Line]
 
     def parse(self, input_lines: list[str]):
         """Parse line:
         Sensor at x=2, y=0: closest beacon is at x=2, y=10
         """
-        self.sensors = []
-        self.beacons = []
-        self.distance = []
+        self.lines = []
         for line in input_lines:
             p = parse(
                 "Sensor at x={sx:d}, y={sy:d}: closest beacon is at x={bx:d}, y={by:d}",
@@ -24,9 +27,12 @@ class Day15:
             sensor = Point(p["sx"], p["sy"])
             beacon = Point(p["bx"], p["by"])
             distance = abs(sensor.distance(beacon))
-            self.sensors.append(sensor)
-            self.beacons.append(beacon)
-            self.distance.append(distance)
+            self.lines.append(Line(sensor=sensor, beacon=beacon, distance=distance))
+
+    @staticmethod
+    def remaining_on_line(line, y):
+        """Calculate the distance between the sensor and the row we are measuring"""
+        return line.distance - abs(line.sensor.y - y)
 
 
 class Day15PartA(Day15, FileReaderSolution):
@@ -35,24 +41,21 @@ class Day15PartA(Day15, FileReaderSolution):
         # Exclude the beacons on this lines
         beacons_on_row = set()
 
-        for sensor, beacon, distance in zip(self.sensors, self.beacons, self.distance):
-            if beacon.y == row:
+        for line in self.lines:
+            if line.beacon.y == row:
                 # We do not calculate the beacons.
-                beacons_on_row.add(beacon)
+                beacons_on_row.add(line.beacon)
 
             # Calculate the distande between the sensor and the row we are measuring
-            if sensor.y > row:
-                remainder = distance - (sensor.y - row)
-            else:
-                remainder = distance - (row - sensor.y)
+            remainder = self.remaining_on_line(line, row)
 
             # Skip sensors too far away
             if remainder < 0:
                 continue
 
             for i in range(remainder + 1):
-                invalid_points.add(sensor.x + i)
-                invalid_points.add(sensor.x - i)
+                invalid_points.add(line.sensor.x + i)
+                invalid_points.add(line.sensor.x - i)
 
         return len(invalid_points) - len(beacons_on_row)
 
@@ -67,11 +70,40 @@ class Day15PartA(Day15, FileReaderSolution):
 class Day15PartB(Day15, FileReaderSolution):
     def find_frequency(self, factor: int) -> int:
         """Find the tuning frequency"""
-        return -1
+        y = 0
+        while y < factor:
+            print(f"New line {y}")
+            x = 0
+            while x <= factor:
+                # print(f"new {x=}")
+                new_x = x
+                for line in self.lines:
+                    new_x = self.skip_over_line(line, x, y)
+                    # print(f"{new_x=}")
+                    if x != new_x:
+                        break
+                if new_x == x:
+                    # Return here?
+                    res = (x * factor) + y
+                    print(f"Result ?? {res=}")
+                    y = factor + 1
+                    break
+                x = new_x
+            y += 1
+        return (x * factor) + y
+
+    def skip_over_line(self, line, x, y):
+        remaining = self.remaining_on_line(line, y)
+        if x >= line.sensor.x - remaining and x <= line.sensor.x + remaining:
+            return line.sensor.x + 1
+        return x
+        # if line[0].x - remaining <= x <= line[0].x + remaining:
+        #     return line[0].x + remaining + 1
+        # return x
 
     def execute(self, input_data: str, factor: int) -> int:
         self.parse(input_data.splitlines())
         return self.find_frequency(factor)
 
     def solve(self, input_data: str) -> int:
-        return self.execute(input_data, factor=4000000)
+        return self.execute(input_data, factor=4_000_000)
